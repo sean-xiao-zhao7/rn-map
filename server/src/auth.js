2d2e91b509d175cccf152a71320a3dfe310b3fdb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-12 11:44:25 -0400

    Uncommitted changes

--- server/src/auth.js
+++ server/src/auth.js
@@ -3,6 +3,8 @@ import { dbClient } from "./db.js";
 
 const authRoutes = express.Router();
 
-authRoutes.post("/register", (req, res) => {});
+authRoutes.post("/register", (req, res) => {
+    console.log(req);
+});
 
 export default authRoutes;
