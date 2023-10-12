0f58d0fc2646073df3415ad4e6f4d453660451b7
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-12 13:44:27 -0400

    Add requireAuth logic
    Decode JWT

--- server/src/middlewares/require-auth.js
+++ server/src/middlewares/require-auth.js
@@ -2,5 +2,20 @@ import { jwt } from "jsonwebtoken";
 import mongoose from "mongoose";
 
 import { User } from "../models/User.js";
+import { jwta } from "../../a.js";
 
-export const requireAuthMiddleware = (req, res, next) => {};
+export const requireAuthMiddleware = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        return res.sentStatus(401);
+    }
+
+    const jwToken = authorization.replace("Bearer ", "");
+    jwt.verify(jwToken, jwta, async (err, payload) => {
+        if (err) {
+            return res.sentStatus(401);
+        }
+
+        const { userId } = payload;
+    });
+};
