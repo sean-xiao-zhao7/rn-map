a2fc4c76c86301aae5ba2f58e50998c92c2c65e0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-12 12:44:15 -0400

    Add register route logic

--- server/src/auth.js
+++ server/src/auth.js
@@ -1,10 +1,23 @@
 import express from "express";
+import { User } from "./models/User.js";
 
 const authRoutes = express.Router();
 
-authRoutes.post("/register", (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
+authRoutes.post("/register", async (req, res) => {
+    try {
+        console.log(req.body);
+        const email = req.body.email;
+        const password = req.body.password;
+        const firstname = req.body.firstname;
+        const lastname = req.body.lastname;
+        const newUser = new User(email, password, firstname, lastname);
+        await newUser.save();
+        res.status = 200;
+        res.send("User added.");
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 });
 
 export default authRoutes;
--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,19 @@
+// external imports
 import express from "express";
 import bodyParser from "body-parser";
-
-import authRoutes from "./auth.js";
-
 import mongoose from "mongoose";
+
+// custom imports
+import authRoutes from "./auth.js";
 import { uri } from "../a.js";
 
 mongoose.connect(uri);
-
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Map REST API.");
+    res.send("Maps REST API.");
 });
 
 app.listen(5000, async () => {});
