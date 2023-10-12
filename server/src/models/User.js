987840e4fc838b083969d85ead7339bf12b5db66
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-12 12:22:47 -0400

    Make user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-export const userSchema = new Schema({
+const userSchema = new Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, unique: true, required: true },
     firstname: { type: String, unique: false, required: true },
     lastname: { type: String, unique: false, required: true },
 });
+
+export const User = mongoose.model("User", userSchema);
