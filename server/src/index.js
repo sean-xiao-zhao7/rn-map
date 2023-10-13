790d2aa873830d0612db03023929994fbfa65bdd
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-13 12:14:29 -0400

    Hook up tripsRoutes

--- server/src/index.js
+++ server/src/index.js
@@ -4,14 +4,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
 // custom imports
-import authRoutes from "./auth.js";
 import { uri } from "../a.js";
 import { requireAuthMiddleware } from "./middlewares/require-auth.js";
+import authRoutes from "./auth.js";
+import tripsRoutes from "./trips.js";
 
 mongoose.connect(uri);
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(tripsRoutes);
 
 app.get("/", requireAuthMiddleware, (req, res) => {
     // console.log(req.user);
---
+++ server/src/trips.js
@@ -0,0 +1,86 @@
+import express from "express";
+
+import { User } from "./models/User.js";
+
+const tripsRoutes = express.Router();
+
+tripsRoutes.post("/start", async (req, res) => {
+    try {
+        // add user
+        const email = req.body.email;
+        const password = req.body.password;
+        const firstname = req.body.firstname;
+        const lastname = req.body.lastname;
+        const newUser = new User({ email, password, firstname, lastname });
+        await newUser.save();
+
+        // return jwt
+        const newToken = jwt.sign(
+            {
+                userId: newUser._id,
+            },
+            jwta
+        );
+
+        res.status = 200;
+        res.send({ message: "New user registered.", jwt: newToken });
+    } catch (error) {
+        console.log(error);
+        if (error.message.includes("is required")) {
+            res.status = 422;
+            res.send("Some required fields are empty.");
+        } else if (error.message.includes("email_1 dup key")) {
+            res.status = 422;
+            res.send(`Email ${req.body.email} is already registered.`);
+        } else {
+            res.status = 500;
+            res.send("Server error.");
+        }
+    }
+});
+
+tripsRoutes.post("/add-coordinate", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.sendStatus(404);
+        }
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(422);
+            }
+            // console.log(isMatch);
+            if (!isMatch) {
+                res.status(401);
+                return res.send("Incorrect password.");
+            } else {
+                // return jwt
+                const newToken = jwt.sign(
+                    {
+                        userId: user._id,
+                    },
+                    jwta
+                );
+
+                res.status = 200;
+                res.send({ message: "User logged in.", jwt: newToken });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        if (error.message.includes("is required")) {
+            res.status = 422;
+            res.send("Some required fields are empty.");
+        } else {
+            res.status = 500;
+            res.send("Server error.");
+        }
+    }
+});
+
+export default tripsRoutes;
