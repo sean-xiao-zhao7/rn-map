865ec4edce1e41a735256f5ecd7ce8445d8f6e53
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-16 12:27:20 -0400

    Add base screens

---
+++ src/screens/TracksScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const TracksScreen = () => {
+    return (
+        <View>
+            <Text>Tracks Screen</Text>
+        </View>
+    );
+};
+
+export default TracksScreen;
