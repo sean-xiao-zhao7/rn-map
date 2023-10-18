3409bbd330df646784f9a51c7b8612d961ff6356
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-18 12:17:59 -0400

    Add completed createDataContext

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,5 +1,26 @@
-import { userReducer } from "react";
+import { useReducer } from "react";
 
 export default (reducer, actions, defaultState) => {
     const Context = React.createContext();
+
+    const Provider = (children) => {
+        const [state, dispatch] = useReducer(reducer, defaultState);
+
+        const boundActions = {};
+
+        for (const key in actions) {
+            boundActions[key] = actions[key](dispatch);
+        }
+
+        return (
+            <Context.Provider value={{ state, ...boundActions }}>
+                {children}
+            </Context.Provider>
+        );
+    };
+
+    return {
+        Context,
+        Provider,
+    };
 };
